61587426900b1fbad0eee401998679c1b5df23b8
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-07-08 00:14:51 +0200

    Delete EXP3_RecommendArm.m

--- EXP3_RecommendArm.m
+++
@@ -1,7 +0,0 @@
-function [ArmToPlay, p] = EXP3_RecommendArm(w, gamma)
-    K = length(w);
-    p = (1-gamma)*w/sum(w) + gamma/K;
-    Proba = cumsum(p);
-    ArmToPlay = Proba > rand();
-    ArmToPlay = find(ArmToPlay == 1,1,'first');
-end
